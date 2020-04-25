f7f9226cb348e2ba2d2fa2fb01257294a3594afc
mcapriotti <mcapriotti@ufl.edu>
2020-04-25 13:47:56 -0400

    initial commit

---
+++ AnonApp/UpcomingChannelCells.swift
@@ -0,0 +1,29 @@
+//
+//  UpcomingChannelCells.swift
+//  AnonApp
+//
+//  Created by Matthew Capriotti on 4/11/20.
+//  Copyright © 2020 Matthew Capriotti. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingChannelCells: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    
+
+}
+
+class QuipCells:UITableViewCell{
+    
+    @IBOutlet weak var quipText: UILabel!
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var downButton: UIButton!
+    @IBOutlet weak var timePosted: UILabel!
+    @IBOutlet weak var replyButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    
+}
